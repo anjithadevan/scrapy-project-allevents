4b0728123126efc545dd078ef8ffbb3d90cdc475
anjithadevan <anjithadevan@gmail.com>
2017-11-21 20:30:06 +0530

    changed spider_second commit

--- trialproject/spiders/allevents_spider.py
+++ trialproject/spiders/allevents_spider.py
@@ -1,13 +1,21 @@
 import scrapy
+from scrapy import Request
+
+headers = {
+    "user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
+}
 
 
-class MySpider(scrapy.Spider):
-    name = "events"
-    start_urls = [
-        'https://allevents.in/new%20delhi/all',
-    ]
+class EventSpider(scrapy.Spider):
+    name = 'eventspider'
+    start_urls = ['http://allevents.in/new delhi/all']
+
     def parse(self, response):
-        for event in response.css('div'):
-            yield {
-                'name': event.css('h3 a::text').extract_first(),
-            }
+        for event in response.css('.event-body'):
+            yield {'name': event.css('a ::text').extract_first()}
+
+    def start_requests(self):
+        reqs = []
+        for item in self.start_urls:
+          reqs.append(Request(url=item, headers=headers))
+        return reqs
